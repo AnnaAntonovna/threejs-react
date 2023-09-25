0f2187a1a663e4e768d164f0fb0370fd9965ba41
Anna <a.baranova@strategiedigitali.io>
2023-09-25 16:40:53 +0200

    Starts

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import Tilt from 'react-tilt';
+import {Tilt} from 'react-tilt';
 import {motion} from 'framer-motion';
 
 import {styles} from '../styles';
@@ -6,6 +6,48 @@ import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import {fadeIn, textVariant} from '../utils/motion';
+
+const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
+  return(
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+        <Tilt
+          optioins={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        >
+          <div className='relative w-full h-[230px]'>
+            <img src={image} alt={name}  className='w-full h-full object-cover rounded-2xl'/>
+          </div>
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div onClick={() => window.open(source_code_link, '_blank')}
+            
+            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursos-pointer'>
+              <img src={github} alt="github" 
+              className='w-1/2 h-1/2 object-contain'/>
+            </div> 
+          </div>
+          
+          <div className='mt-5'>
+            <h3 className='text-white font-bold text-[24px]'
+            >{name}</h3>
+            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          </div>
+          <div className='mt-4 flex flex-wrap gap-2'>
+            {tags.map((tag) => 
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+            )}
+          </div>
+
+        </Tilt>
+    </motion.div>
+  )
+}
 
 
 
@@ -16,6 +58,25 @@ const Works = () => {
         <p className={styles.sectionSubText}>My projects</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Here are aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa projects
+          offfffffffffffffffffffffff ffffffffff ffffffff ffffffff ffffffff
+          meeeeeeeeeee mememmememem they show how cool I am
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+      </div>
     </>
   )
 }
